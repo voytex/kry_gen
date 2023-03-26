e433724d95ffe8c9017e07f52114a16c6dbb5d2b
Vojtěch Lukáš <vlukas@hotmail.cz>
2023-03-26 20:25:24 +0200

    bits and pieces

--- front-end/main.py
+++ front-end/main.py
@@ -1,10 +1,17 @@
 import requests, json
 
-WELCOME_STRING = "Crypto Tasks - ukázková verze\nZadejte kód úlohy, kterou si přejete řešit"
+WELCOME_STRING = "Crypto Tasks - ukázková verze\nZadejte kód úlohy, kterou si přejete řešit\n\nDosavadní úlohy:"
 
 API = "http://vut-fekt-mpckry-gr14.8u.cz/index.php"
 
 print(WELCOME_STRING)
+
+request = requests.get(f"{API}/alltasks")
+
+json_req = request.json()
+
+for row in json_req:
+    print(f"- {row['description']}")
 
 code = str(input())
 
