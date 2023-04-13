7942613810d1fe8d2a8d04d0582de8f2dd98c733
xchren05 <chrencik.d@gmail.com>
2023-04-13 17:30:49 +0200

    Update main.py

--- front-end/main.py
+++ front-end/main.py
@@ -10,35 +10,37 @@ request = requests.get(f"{API}/alltasks")
 
 json_req = request.json()
 
+valid_codes = [row['code'] for row in json_req]
+
 for row in json_req:
     print(f"{row['code']} - {row['description']}")
 
 while True:
-
     code = str(input("\nZadejte kód úlohy, kterou si přejete řešit: "))
 
-    request = requests.get(f"{API}/task?code={code}")
+    if code not in valid_codes:
+        print("Špatný kód")
+    else:    
+        request = requests.get(f"{API}/task?code={code}")
 
-    json_req = request.json()
-    print(f"{json_req['description']} [? - nápověda], [q - ukončit]")
+        json_req = request.json()
+        print(f"{json_req['description']} [? - nápověda], [q - ukončit]")
 
-    while True:
-        answer = input("\nZadej výsledek: ")
-        if answer == '?':
-            print(json_req['hint'])
-            
-        elif answer.lower() == 'q':
-            show_solution = input("Chceš zobrazit správný výsledek? (y/n): ")
-            if show_solution.lower() == 'y':
-                print(f"\nSprávný výsledek: {json_req['result']}")
-            break    
-            
-        elif (answer == json_req['result']):
-            print("Správně!")
+        while True:
+            answer = input("\nZadej výsledek: ")
+            if answer == '?':
+                print(json_req['hint'])
+            elif answer.lower() == 'q':
+                show_solution = input("Chceš zobrazit správný výsledek? (y/n): ")
+                if show_solution.lower() == 'y':
+                    print(f"\nSprávný výsledek: {json_req['result']}")
+                break    
+            elif (answer == json_req['result']):
+                print("Správně!")
+                break
+            else:
+                print("Špatně, zkus to znovu.")
+                
+        continue_input = input("\nChceš pokračovat v řešení dalšího úkolu? (y/n): ")
+        if continue_input.lower() != 'y':
             break
-        else:
-            print("Špatně, zkus to znovu.")
-            
-    continue_input = input("\nChceš pokračovat v řešení dalšího úkolu? (y/n): ")
-    if continue_input.lower() != 'y':
-        break
