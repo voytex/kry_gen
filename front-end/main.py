2c342e5a0ea42eeb082f4455bdf6dc28952e8ad8
Vojtěch Lukáš <vlukas@hotmail.cz>
2023-04-22 09:59:56 +0200

    minor edit for readability

--- front-end/main.py
+++ front-end/main.py
@@ -11,36 +11,46 @@ print(WELCOME_STRING)
 
 
 while True:
+    # Vyzadani a zobrazeni vsech uloh
     valid_codes = print_all_tasks()
+
+    # Uzivatel vybira ulohu, kterou chce resit
     code = str(input(f"{C_BLUE}[Skóre: {SCORE}] {C_YELLOW}Zadejte kód úlohy, kterou si přejete řešit:{C_RES}"))
 
+
     if code not in valid_codes:
+        # Dokud uzivatel nezada spravny kod,
+        # neni poslan pozadavek na server.
         print(f"{C_RED}Špatný kód{C_RES}")
-    else:    
-        clear_console()
-        request = requests.get(f"{API}/task?code={code}")
+        continue
 
-        json_req = request.json()
-        print(f"{json_req['description']} {C_YELLOW}[? - nápověda], [q - ukončit]{C_RES}")
+    clear_console()
 
-        while True:
-            answer = input("\nZadej výsledek: ")
-            if answer == '?':
-                print(f"{C_YELLOW}{json_req['hint']}{C_RES}")
-            elif answer.lower() == 'q':
-                show_solution = input("Chceš zobrazit správný výsledek? (y/n): ")
-                if show_solution.lower() == 'y':
-                    print(f"\nSprávný výsledek: {C_YELLOW}{json_req['result']}{C_RES}")
-                break    
-            elif (answer == str(json_req['result'])):
-                print(f"{C_GREEN}Správně!{C_RES}")
-                SCORE += 1
-                break
-            else:
-                print(f"{C_RED}Špatně, zkus to znovu.{C_RES}")
-                
-        continue_input = input("\nChceš pokračovat v řešení dalšího úkolu? (y/n): ")
-        if continue_input.lower() != 'y':  
-            print(f"{C_BLUE}GG [Vaše skóre: {SCORE}]{C_RES}")
+    # Vyzadani jedne ulohy 
+    request = requests.get(f"{API}/task?code={code}")
+
+    # Prevedeni z JSON do Python dictionary
+    json_req = request.json()
+    print(f"{json_req['description']} {C_YELLOW}[? - nápověda], [q - ukončit]{C_RES}")
+
+    while True:
+        answer = input("\nZadej výsledek: ")
+        if answer == '?':
+            print(f"{C_YELLOW}{json_req['hint']}{C_RES}")
+        elif answer.lower() == 'q':
+            show_solution = input("Chceš zobrazit správný výsledek? (y/n): ")
+            if show_solution.lower() == 'y':
+                print(f"\nSprávný výsledek: {C_YELLOW}{json_req['result']}{C_RES}")
+            break    
+        elif (answer == str(json_req['result'])):
+            print(f"{C_GREEN}Správně!{C_RES}")
+            SCORE += 1
             break
-        clear_console()
+        else:
+            print(f"{C_RED}Špatně, zkus to znovu.{C_RES}")
+            
+    continue_input = input("\nChceš pokračovat v řešení dalšího úkolu? (y/n): ")
+    if continue_input.lower() != 'y':  
+        print(f"{C_BLUE}GG [Vaše skóre: {SCORE}]{C_RES}")
+        break
+    clear_console()
