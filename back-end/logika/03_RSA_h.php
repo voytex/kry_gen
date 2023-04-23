959de6296d5a4ec5e5951cb6e819c3bc0963249d
Vojtěch Lukáš <vlukas@hotmail.cz>
2023-04-23 11:33:20 +0200

    feedback pls

--- back-end/logika/03_RSA_h.php
+++ back-end/logika/03_RSA_h.php
@@ -6,23 +6,23 @@
 
 //funguje to jen pro relativne mala cisla, jakmile jsou zprava nebo klice moc velke, zacina se to jebat
 
-$m = intval(gmp_nextprime(rand(4, 18))); //zprava m
+$m = intval(gmp_nextprime(rand(4, 30))); //zprava m
 
 for ($k=1; $k=1000; $k++) {
 
-$p = intval(rand(2, 17)); //stanoveni zakladnich hodnot p,q,n,phi
-$q = intval(rand(2, 17));
+$p = intval(gmp_nextprime(rand(2, 50))); //stanoveni zakladnich hodnot p,q,n,phi
+$q = intval(gmp_nextprime(rand(2, 50)));
 $n = $p * $q;
 $phi = ($p - 1)*($q - 1);
 
-$e = intval(rand(2, 20)); //verejny klic e
+$e = intval(rand(2, $phi)); //verejny klic e
 $d = intval(gmp_invert($e, $phi)); //soukromy klic d
+
 $c = intval(pow($m, $e) % $n); //kryptogram c
+$m1 = intval(pow($c, $d) % $n); //decrypt zpatky na m ($m1 slozi jako kontrola)
 
-$m1 = intval(pow($c, $d) % $n); //pouzite v podmince nize, bez te podminky to funguje uplne na random
-
-if ($e < $phi and $e != $d and $e % $phi != 0 and $c > 1 and $m == $m1 and $m != $c){
-    break;
+if ($e < $phi and $e != $d and $e % $phi != 0 and $c > 1 and $m == $m1 and $m != $c and $n % $e != 0){
+    return[$m, $p, $q, $e, $d, $c];
 }
 
 }
